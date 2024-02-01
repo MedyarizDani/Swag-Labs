83cd774bd2e1e5138efc353e1e140dd53bf8b866
mrdi_ <mrdi_@DHANIE-PC>
2024-02-01 14:16:12 +0700

    commit

---
+++ Test Suites/Main Test Suites/Checkout Test Suites v1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Checkout Test Suites v1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1be9c2ce-590d-4fb2-989a-ce3eaffeb001</testSuiteGuid>
+   <testCaseLink>
+      <guid>bfe8ab96-b15b-4727-8d7d-2d3e17b95b4c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Case/Checkout Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
